6e3d230682973b3bcb15dd03cbad8400c178a646
maitebarreravillarias <145692501+maitebarreravillarias@users.noreply.github.com>
2024-03-10 22:35:54 +0100

    Add files via upload

--- 2_Pair_programming_queries_avanzadas.sql
+++ 2_Pair_programming_queries_avanzadas.sql
@@ -1,4 +1,4 @@
-USE tienda_northwind
+USE NORTHWIND
 
 -- 1. Productos más baratos y caros de nuestra BBDD:
 -- Desde la división de productos nos piden conocer el precio de los productos que tienen el precio más alto y más bajo. 
@@ -87,7 +87,20 @@ FROM orders;
 
 SELECT COUNT(order_id), MAX(freight), employee_id
 FROM orders
-GROUP BY employee_id
+GROUP BY employee_id;
+
+/* 8. Descartar pedidos sin fecha y ordénalos:
+Una vez han revisado los datos de la consulta anterior, nos han pedido afinar un poco más el "disparo". En el resultado anterior se han incluido muchos
+pedidos cuya fecha de envío estaba vacía, por lo que tenemos que mejorar la consulta en este aspecto. También nos piden que ordenemos los resultados
+según el id del empleado para que la visualización sea más sencilla.
+*/
+
+
+SELECT COUNT(order_id), MAX(freight), employee_id, shipped_date
+FROM orders
+WHERE shipped_date IS NOT NULL
+GROUP BY employee_id, shipped_date;
+
 
 /* 9. Números de pedidos por día:
 El siguiente paso en el análisis de los pedidos va a consistir en conocer mejor la distribución de los mismos según las fechas. 
@@ -96,8 +109,48 @@ Por lo tanto, tendremos que generar una consulta que nos saque el número de ped
 */
  
  SELECT COUNT(order_id)
- FROM ORDERS
+ FROM orders
  
- SELECT *
- FROM ORDERS
+ SELECT order_date
+ FROM orders
+ LIMIT 10;
  
+SELECT 
+    COUNT(order_id) AS order_count, 
+    DATE(order_date) AS order_day,
+    MONTH(order_date) AS order_month,
+    YEAR(order_date) AS order_year
+FROM orders
+GROUP BY order_day, order_month, order_year;
+
+/* 10. Número de pedidos por mes y año:
+La consulta anterior nos muestra el número de pedidos para cada día concreto, pero esto es demasiado detalle. Genera una modificación de 
+la consulta anterior para que agrupe los pedidos por cada mes concreto de cada año. */
+
+SELECT 
+    COUNT(order_id) AS order_count, 
+    MONTH(order_date) AS order_month,
+    YEAR(order_date) AS order_year
+FROM orders
+GROUP BY order_month, order_year;
+
+
+/* 11. Seleccionad las ciudades con 4 o más empleadas:
+Desde recursos humanos nos piden seleccionar los nombres de las ciudades con 4 o más empleadas de cara a estudiar la apertura de nuevas oficinas*/
+
+SELECT city, COUNT(employee_id)
+FROM employees
+GROUP BY city
+HAVING  COUNT(employee_id) > 4;
+
+
+/* 12. Cread una nueva columna basándonos en la cantidad monetaria:
+Necesitamos una consulta que clasifique los pedidos en dos categorías ("Alto" y "Bajo") en función de la cantidad monetaria total que han supuesto:
+por encima o por debajo de 2000 euros*/
+
+SELECT order_id, (quantity * unit_price), 
+CASE
+	WHEN (quantity * unit_price) > 2000 THEN "Alto"
+    WHEN (quantity * unit_price) < 2000 THEN "Bajo"
+    END AS clasificacion_pedidos
+FROM order_details;   
