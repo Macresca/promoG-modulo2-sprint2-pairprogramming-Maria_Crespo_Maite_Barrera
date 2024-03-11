ce77f1201198675407f8a92f3f97a5749ea2d841
macresca <mariacrespocalabuig@gmail.com>
2024-03-11 21:49:38 +0100

    update pair

--- 3_Pair_programming_union_tables.sql
+++ 3_Pair_programming_union_tables.sql
@@ -1,4 +1,4 @@
-USE tienda_northwind;
+USE northwind;
 
 /* 1 Pedidos por empresa en UK:
 Desde las oficinas en UK nos han pedido con urgencia que realicemos una consulta a la base de datos con la que podamos 
@@ -82,3 +82,76 @@ FROM orders AS pd
 WHERE ship_country = 'UK'
 GROUP BY YEAR(order_date),customer_id, company_name;
 
+/* 4.BONUS: Pedidos que han realizado cada compañía y su fecha:
+Después de estas solicitudes desde UK y gracias a la utilidad de los resultados que se han obtenido, 
+desde la central nos han pedido una consulta que indique el nombre de cada compañia cliente junto con 
+cada pedido que han realizado y su fecha.
+*/
+
+SELECT order_id, company_name, order_date
+FROM orders
+INNER JOIN customers
+ON orders.customer_id = customers.customer_id;
+
+/*5.BONUS: Tipos de producto vendidos:
+Ahora nos piden una lista con cada tipo de producto que se han vendido, sus categorías, nombre de la categoría y 
+el nombre del producto, y el total de dinero por el que se ha vendido cada tipo de producto (teniendo en cuenta los descuentos).
+Pista Necesitaréis usar 3 joins
+*/
+
+SELECT categories.category_id, categories.category_name,products.product_name, ROUND(SUM(order_details.unit_price* order_details.quantity*(1- order_details.discount)),2) AS ProductSales
+FROM categories
+INNER JOIN products
+	ON categories.category_id = products.category_id
+INNER JOIN order_details
+	ON products.product_id = order_details.product_id
+GROUP BY categories.category_id, categories.category_name,products.product_name;
+
+/* 6. Qué empresas tenemos en la BBDD Northwind:
+Lo primero que queremos hacer es obtener una consulta SQL que nos devuelva el nombre de todas las empresas cliente,
+ los ID de sus pedidos y las fechas.
+*/
+
+SELECT orders.order_id, orders.order_date,customers.company_name
+FROM orders
+INNER JOIN customers
+ON orders.customer_id = customers.customer_id;
+
+/* 7. Pedidos por cliente de UK:
+Desde la oficina de Reino Unido (UK) nos solicitan información acerca del número de 
+pedidos que ha realizado cada cliente del propio Reino Unido de cara a conocerlos mejor y poder adaptarse al mercado actual. 
+Especificamente nos piden el nombre de cada compañía cliente junto con el número de pedidos.
+*/
+SELECT customers.company_name, COUNT(orders.order_id) AS 'Pedido'
+FROM orders
+INNER JOIN customers
+ON orders.customer_id = customers.customer_id
+WHERE customers.country = "UK"
+GROUP BY company_name;
+
+/* 8. Empresas de UK y sus pedidos:
+También nos han pedido que obtengamos todos los nombres de las empresas cliente de Reino Unido (tengan pedidos o no) 
+junto con los ID de todos los pedidos que han realizado y la fecha del pedido.
+*/
+
+SELECT company_name, order_id, order_date
+FROM customers
+LEFT JOIN orders
+ON customers.customer_id = orders.customer_id
+WHERE country = 'UK';
+
+/* 9. Empleadas que sean de la misma ciudad:
+Ejercicio de SELF JOIN: Desde recursos humanos nos piden realizar una consulta que muestre por pantalla los datos de todas las empleadas y sus supervisoras. 
+Concretamente nos piden: la ubicación, nombre, y apellido tanto de las empleadas como de las jefas. Investiga el resultado, ¿sabes decir quién es el director?
+*/
+
+SELECT 	E.city AS 'Ciudad_ Empleado', 
+		E.first_name AS 'Nombre_Empleado', 
+        E.last_name AS 'Apellido_Empleado', 
+        M.city AS 'Ciudad_ Manager', 
+        M.first_name AS 'Nombre_Manager', 
+        M.last_name AS 'Apellido_Manager'
+FROM employees AS E
+JOIN employees AS M
+	ON E.title NOT LIKE M.title
+WHERE M.title LIKE '%Manager%';
--- 5_Pair_programming_subconsultas.sql
+++ 5_Pair_programming_subconsultas.sql
@@ -1,3 +1,28 @@
+
+/* 1. Extraed los pedidos con el máximo "order_date" para cada empleado.
+Nuestro jefe quiere saber la fecha de los pedidos más recientes que ha gestionado cada empleado. 
+Para eso nos pide que lo hagamos con una query correlacionada.
+*/
+
+SELECT `order_id`, `customer_id`, `employee_id`, `order_date`, `required_date`
+	FROM `orders` AS `o1`
+    WHERE `order_date` IN (SELECT MAX(`order_date`)
+									FROM `orders` AS `o2`
+                                    WHERE  `o1`.`employee_id`= `o2`.`employee_id`);
+
+/* 2. Extraed el precio unitario máximo (unit_price) de cada producto vendido.
+Supongamos que ahora nuestro jefe quiere un informe de los productos vendidos y su precio unitario. 
+De nuevo lo tendréis que hacer con queries correlacionadas.
+*/
+
+SELECT `od1`.`product_id`, `od1`.`unit_price` AS "Maxunitprice"
+	FROM `order_details` AS `od1`
+    WHERE `od1`.`unit_price` IN (SELECT MAX(`od2`.`unit_price`)
+					FROM `order_details` AS `od2`
+					WHERE `od1`.`product_id` = `od2`.`product_id`)
+GROUP BY `od1`.`product_id`,`od1`.`unit_price`;
+
+
 /*
 3. Extraed información de los productos "Beverages"
 En este caso nuestro jefe nos pide que le devolvamos toda la información necesaria para identificar un tipo de producto. 
@@ -33,8 +58,15 @@ Extraed el OrderId y el nombre del cliente que pidieron más de 20 artículos de
  WHERE product_id = 6 AND quantity >20);
  
  
- 
- 
- 
- 
+ /* 6. Extraed los 10 productos más caros
+Nos siguen pidiendo más queries correlacionadas. En este caso queremos saber cuáles son los 10 productos más caros.
+*/
+
+SELECT O1.product_name, O1.unit_price
+FROM products AS O1
+WHERE O1.unit_price IN (
+    SELECT O2.unit_price
+    FROM products AS O2
+    WHERE O1.product_id = O2.product_id)
+LIMIT 10;
  
